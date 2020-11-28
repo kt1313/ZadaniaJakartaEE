58c63845de9f59729a1931498ffee79642890425
kt1313 <tomaklim@wp.pl>
2020-11-28 17:27:17 +0100

    Zad YTplayer - poczatek

---
+++ src/main/java/pl/javastart/YTplayer.java
@@ -0,0 +1,13 @@
+package pl.javastart;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+@WebServlet("")
+public class YTplayer extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+}
