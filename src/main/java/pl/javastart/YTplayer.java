56a7bd67f0185b78d802b2cfb993457adfaafb7b
kt1313 <tomaklim@wp.pl>
2020-12-01 07:35:31 +0100

    Zad YTPlayer - koniec

--- src/main/java/pl/javastart/YTplayer.java
+++ src/main/java/pl/javastart/YTplayer.java
@@ -3,7 +3,7 @@ package pl.javastart;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
-@WebServlet("")
+@WebServlet("/ytplayer")
 public class YTplayer extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
